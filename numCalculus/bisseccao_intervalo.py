8d4d5076c69d022362beafd12706ceed9175a44e
pedlobs <pedlobs@gmail.com>
2022-06-20 00:08:17 -0300

    Adding comments to bissection file

--- numCalculus/bisseccao_intervalo.py
+++ numCalculus/bisseccao_intervalo.py
@@ -1,25 +1,30 @@
 import numpy as np
 
-a = int(input('Limite inferior: '))
-b = int(input('Limite superior: '))
-it = 0
+# Creates the upper and lower limits variables
+a = int(input('Limite inferior: ')) # Lower limit
+b = int(input('Limite superior: ')) # Upper limit
+it = 0 
 
+# Defines a function to give a function to approximate the root
 def f(x):
     return x**3 - 9*x + 3
-while b - a > 0.00001:
+while b - a > 0.00001: # Defines the precision of the root approximated
 
-    x = np.mean([a,b])
+    x = np.mean([a,b]) # Defines a new limit 
+                        
+                        
+    if f(a) > 0:        
+        if f(x) > 0:        
+            a = x   # if the value of the function on the new limit       
+        else:       # is positive, the new value updates the upper limit                
+            b = x   # if it is negative, it updates the lower limit   
+    else:           
+        if f(x) > 0:
+            b = x
+        else:
+            a = x
+    it+=1 # Updates the number of iterations
 
-    if f(a) > 0:
-        if f(x) > 0:
-            a = x
-        else:
-            b = x
-    else:
-        if f(x) > 0:
-            b = x
-        else:
-            a = x
-    it+=1    
+# Prints the approximated root and the number of iterations
 print('Raiz: {}'.format(x))
 print('Iterações: {}'.format(it))
